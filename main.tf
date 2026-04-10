c1923b28b16b8a8862b4726e152d69f0b7066f56
Ravichandu-git <chandub91335@gmail.com>
2026-04-10 12:10:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-
-data "aws_secretsmanager_secret_version" "ec2_secret" {
-  secret_id = "prod"
-}
-
-locals {
-  secret_data = jsondecode(data.aws_secretsmanager_secret_version.ec2_secret.secret_string)
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0861f4e788f5069dd"  # Replace with valid AMI
-  instance_type = "t3.micro"
+resource "aws_instance" "first_instance" {
+  ami           = "ami-05d2d839d4f73aafb"
+  instance_type = "t2.micro"
   key_name      = "august-2025"
 
-}
-
-output "key_used" {
-  value = "august-2025"
-}
-
-output "secret_debug" {
-  value     = local.secret_data
-  sensitive = true
+  tags = {
+    Name = "my-ec2-instance"
+  }
 }
