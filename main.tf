61e5bf46f116160ba40862751b8031133d0d014b
Ravichandu-git <chandub91335@gmail.com>
2026-07-09 23:20:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "first_instance" {
   ami           = "ami-01a00762f46d584a1"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "julykey-2026"
 
   tags = {
