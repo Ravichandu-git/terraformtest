908cc4ef1e20bf97f88ae34d1169511e24f0375b
Ravichandu-git <chandub91335@gmail.com>
2026-07-09 22:55:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,19 @@ provider "aws" {
 resource "aws_instance" "first_instance" {
   ami           = "ami-05d2d839d4f73aafb"
   instance_type = "t2.micro"
-  key_name      = "august-2025"
+  key_name      = "julykey-2026"
 
   tags = {
     Name = "test-instance"
   }
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-demo-bucket-2026-12345"
+
+  tags = {
+    Name        = "my-terraform-demo-bucket"
+    Environment = "Dev"
+  }
+}
+
